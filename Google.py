debe8fb398bddedeff4d899b8c2e9ac3a812413c
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-08-19 06:49:36 +0700

    Update Google.py

--- Google.py
+++ Google.py
@@ -38,8 +38,8 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
                         "token_uri":"https://oauth2.googleapis.com/token",
                         "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
                         "client_secret":os.getenv("client_secret"),
-                        "redirect_uri": "localhost:8080/auth/google",
-                        "proxy": "true"
+                        "redirect_uris": ["http://localhost:51857/","http://localhost:8080/"]
+                       
                         }
                     },
                 scopes=SCOPES
