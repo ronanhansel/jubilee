32a6f6a7220193c99cfb5ab7daee06e78fd3267a
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-08-18 23:57:27 +0700

    Update Google.py

--- Google.py
+++ Google.py
@@ -37,7 +37,8 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
                         "auth_uri":"https://accounts.google.com/o/oauth2/auth",
                         "token_uri":"https://oauth2.googleapis.com/token",
                         "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-                        "client_secret":os.getenv("client_secret")
+                        "client_secret":os.getenv("client_secret"),
+                        "scopes": SCOPES
                         }
                     }
 
