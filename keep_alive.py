208af16007017fe4212baa77f96bba619bef88bb
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-08-19 09:57:52 +0700

    fixed port

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "Hello. I am alive!"
 
 def run():
-  app.run(host='0.0.0.0',port=3000)
+  app.run(host='0.0.0.0',port=1080)
 
 def keep_alive():
     t = Thread(target=run)
--- new.py
+++
@@ -1,3 +0,0 @@
-from keep_alive import keep_alive
-
-keep_alive()
