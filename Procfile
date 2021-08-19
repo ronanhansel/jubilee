7edc8cf71186530babb48137c180cd022545e4ef
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-08-19 20:35:00 +0700

    reverted changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+worker: python main.py
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 from discord.ext import commands
 import os
-#from app_placeholder import tracker
-from flask import Flask 
-
-  
-
-app = Flask(__name__) 
-  
-@app.route("/") 
-
-def home_view(): 
-  return "<h1>Welcome to Juvenile's placeholder</h1>"
-
 
 #Import and load all files
-
-
-
 client = commands.Bot(command_prefix= "-")
 client.remove_command('remove')
 @client.command(hidden=True)
@@ -42,5 +27,4 @@ for filename in os.listdir('./cogs'):
   if filename.endswith('py'):
     client.load_extension(f'cogs.{filename[:-3]}')
 
-#tracker()
 client.run(os.getenv('discord_token'))
