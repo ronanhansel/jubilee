4837dac23e4e8f779833e27406506d8eba4be5b3
ronanhansel <ronanhansel@gmail.com>
2021-08-19 20:04:50 +0700

    fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import os
-from app_placeholder import tracker
+#from app_placeholder import tracker
 from flask import Flask 
 
   
@@ -42,5 +42,5 @@ for filename in os.listdir('./cogs'):
   if filename.endswith('py'):
     client.load_extension(f'cogs.{filename[:-3]}')
 
-tracker()
+#tracker()
 client.run(os.getenv('discord_token'))
