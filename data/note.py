4df17df1d1fb735fdc7e6d3f3b2da84e5667908c
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-08-20 15:57:23 +0700

    cloud db

--- data/note.py
+++ data/note.py
@@ -1,6 +1,13 @@
-import _sqlite3
+import psycopg2
+import os
 
-conn = _sqlite3.connect('data/note.db')
+
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 c = conn.cursor()
 
